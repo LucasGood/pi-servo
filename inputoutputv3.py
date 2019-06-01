0d92ae63527440fd64b73acf38af594166c92fcc
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-01 18:21:35 -0500

    reverting to previous change

--- inputoutputv3.py
+++ inputoutputv3.py
@@ -5,21 +5,19 @@ import time
 output = 17
 # Set GPIO into BCM mode
 GPIO.setmode(GPIO.BCM)
-# Setup GPIO to output on pin <output>
+#setup GPIO to output on pin <output>
 GPIO.setup(output, GPIO.OUT)
-# Setup GPIO input pin
-GPIO.setup(input, GPIO.IN)
 # Set output PWM to 50 Hz
-out = GPIO.out(output, 50)
+pwm = GPIO.PWM(output, 50)
 # Duty Cycle Start Value
 start = 2.5
 # Initialize output with duty cycle of X%
-out.start(start)
+pwm.start(start)
 
 try:
     while True:
         while start < 10:
-            out.ChangeDutyCycle(start + 1)
+            pwm.ChangeDutyCycle(start + 1)
             time.sleep(0.5)
             start = start + 1
             print(start)
@@ -27,5 +25,5 @@ try:
             start = 2.5
 
 except KeyboardInterrupt:
-  out.stop()
+  pwm.stop()
   GPIO.cleanup()
