72d2a7538d4211637bf1d3bb414a83b5a3b5c931
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-01 18:18:58 -0500

    cycling w/ loop & input

--- inputoutputv3.py
+++ inputoutputv3.py
@@ -3,25 +3,21 @@ import time
 
 # Set output pin
 output = 17
-#Set input pin
-input = 18
 # Set GPIO into BCM mode
 GPIO.setmode(GPIO.BCM)
 # Setup GPIO to output on pin <output>
 GPIO.setup(output, GPIO.OUT)
 # Setup GPIO input pin
-GPIO.setup(input, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(input, GPIO.IN)
 # Set output PWM to 50 Hz
-out = GPIO.OUT(output, 50)
-# Set input
-inp = GPIO.IN(input)
+out = GPIO.out(output, 50)
 # Duty Cycle Start Value
 start = 2.5
 # Initialize output with duty cycle of X%
 out.start(start)
 
 try:
-    while inp == True:
+    while True:
         while start < 10:
             out.ChangeDutyCycle(start + 1)
             time.sleep(0.5)
