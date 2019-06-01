63d6104d4ec13418999087a2198f8f5060b3df98
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-01 14:29:43 -0500

    experimenting w/ servo range

--- inputoutputv2.py
+++ inputoutputv2.py
@@ -14,11 +14,27 @@ GPIO.setup(output, GPIO.OUT)
 pwm = GPIO.PWM(output, 50)
 
 # Initialize output with duty cycle of X%
-pwm.start(2.5)
+pwm.start(0)
 
 try:
     while True:
+        pwm.ChangeDutyCycle(1)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(2)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(3)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(4)
+        time.sleep(0.5)
         pwm.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(6)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(7)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(8)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(9)
         time.sleep(0.5)
         pwm.ChangeDutyCycle(10)
         time.sleep(0.5)
