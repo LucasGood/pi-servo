0f665cc87b2b83511bdbd57d16c2300d4a5e125d
LucasGood <gooddinglucas@gmail.com>
2019-06-01 14:20:00 -0500

    altering naming conventions

--- inputoutputv2.py
+++ inputoutputv2.py
@@ -1,15 +1,35 @@
 import RPi.GPIO as GPIO
 import time
-from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Set output pin
+output = 17
 
+# Set GPIO into BCM mode
+GPIO.setmode(GPIO.BCM)
 
-while True:
-    input = GPIO.input(16)
-    if (input == True):
-        print("input true")
-    else:
-        print("input false")
-        
+#setup GPIO to output on pin <output>
+GPIO.setup(output, GPIO.OUT)
+
+# Set output PWM to 50 Hz
+pwm = GPIO.PWM(output, 50)
+
+# Initialize output with duty cycle of X%
+pwm.start(2.5)
+
+try:
+    while True:
+        pwm.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(15)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(20)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(25)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(30)
+
+except KeyboardInterrupt:
+  pwm.stop()
+  GPIO.cleanup()
