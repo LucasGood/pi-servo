8ef9584cdea6f0feb550f2c102c24045793b39bf
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-01 14:31:52 -0500

    experimenting w/ servo range

--- inputoutputv2.py
+++ inputoutputv2.py
@@ -3,40 +3,38 @@ import time
 
 # Set output pin
 output = 17
-
 # Set GPIO into BCM mode
 GPIO.setmode(GPIO.BCM)
-
 #setup GPIO to output on pin <output>
 GPIO.setup(output, GPIO.OUT)
-
 # Set output PWM to 50 Hz
 pwm = GPIO.PWM(output, 50)
-
+# Duty Cycle Start Value
+start = 5
 # Initialize output with duty cycle of X%
 pwm.start(0)
 
 try:
     while True:
-        pwm.ChangeDutyCycle(1)
+        pwm.ChangeDutyCycle(start + 1)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(2)
+        pwm.ChangeDutyCycle(start + 2)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(3)
+        pwm.ChangeDutyCycle(start + 3)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(4)
+        pwm.ChangeDutyCycle(start + 4)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(5)
+        pwm.ChangeDutyCycle(start + 5)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(6)
+        pwm.ChangeDutyCycle(start + 6)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(7)
+        pwm.ChangeDutyCycle(start + 7)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(8)
+        pwm.ChangeDutyCycle(start + 8)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(9)
+        pwm.ChangeDutyCycle(start + 9)
         time.sleep(0.5)
-        pwm.ChangeDutyCycle(10)
+        pwm.ChangeDutyCycle(start + 10)
         time.sleep(0.5)
 
 except KeyboardInterrupt:
