ae950f67757354cf7b14becf82238b3f51ab3bd2
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-05 09:26:05 -0500

    using if statements to check input

---
+++ inputoutputv4.1.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO
+import time
+
+# ----OUTPUT----
+# Set output pin
+output = 17
+
+# ----INPUT----
+# Set input pin
+inp = 4
+
+# Set GPIO into BCM mode
+GPIO.setmode(GPIO.BCM)
+
+#setup GPIO to output on pin <output>
+GPIO.setup(output, GPIO.OUT)
+
+# Set output PWM to 50 Hz
+pwm = GPIO.PWM(output, 50)
+
+# Duty Cycle Start Value
+start = 2.5
+
+
+# Setup pin for input
+GPIO.setup(inp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Assign input to variable
+
+input = GPIO.input(inp)
+
+# Initialize output with variable $start
+pwm.start(start)
+
+
+try:
+    # Loop to keep checking input
+    # Will update to edge detection
+    while True:
+        if input == True:
+            pwm.ChangeDutyCycle(3)
+            time.sleep(0.5)
+            print("Input True")
+        else:
+            pwm.ChangeDutyCycle(start + 5)
+            time.sleep(0.5)
+            print("Input False")
+except KeyboardInterrupt:
+        pwm.stop()
+        GPIO.cleanup()
