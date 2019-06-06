75919114a209daeafd1b845e48c05ea1bb65fa39
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-06 17:48:52 -0500

    starting edge detection

---
+++ edge.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+# ----OUTPUT----
+#  pin no.
+output = 17
+
+# ----INPUT----
+#  pin no.
+inp = 4
+
+# Set GPIO into BCM mode
+GPIO.setmode(GPIO.BCM)
+
+#setup GPIO output
+GPIO.setup(output, GPIO.OUT)
+
+# Setup GPIO input
+GPIO.setup(inp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Set output PWM to 50 Hz
+pwm = GPIO.PWM(output, 50)
+
+# Setup edge/event detection
+try:
+    while True:
+        GPIO.wait_for_edge(inp, GPIO.RISING)
+        print("Rise on pin {0} ".format(inp))
+except KeyboardInterrupt:
+    GPIO.cleanup
+    pwm.stop()
