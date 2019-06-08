b78d8a0b3f1fbeb4491940c4a51c4dbe274bca9f
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-08 11:21:42 -0500

    moving servo w/ edging

--- edge.py
+++ edge.py
@@ -26,13 +26,14 @@ pwm = GPIO.PWM(output, 50)
 try:
     pwm.start(3)
     while True:
+        # Set PWM to 4 if input TRUE
         GPIO.wait_for_edge(inp, GPIO.RISING)
         pwm.ChangeDutyCycle(4)
         print("Rise on pin {0} ".format(inp))
         GPIO.wait_for_edge(inp, GPIO.FALLING)
+        # Set PWM to 6 if innput TRUE
         pwm.ChangeDutyCycle(6)
-
-
+        print("Fall on pin {0} ".format(inp))
 except KeyboardInterrupt:
     GPIO.cleanup
-pwm.stop()
+    pwm.stop()
