cd0be1f979a3caddf5de4769e6ec08cac0fac1b4
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-10 21:53:25 -0500

    testing boolean value on edge detection

--- edgev1.1.py
+++ edgev1.1.py
@@ -1,10 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-# ----OUTPUT----
-#  pin no.
-output = 17
-
 # ----INPUT----
 #  pin no.
 inp = 4
@@ -12,17 +8,11 @@ inp = 4
 # Set GPIO into BCM mode
 GPIO.setmode(GPIO.BCM)
 
-#setup GPIO output
-GPIO.setup(output, GPIO.OUT)
-
 # Setup GPIO input
 GPIO.setup(inp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Set output PWM to 50 Hz
-pwm = GPIO.PWM(output, 50)
-
-channel-rise = GPIO.wait_for_edge(inp, GPIO.RISING)
-channel-fall = GPIO.wait_for_edge(inp, GPIO.FALLING)
+channel-rise = GPIO.wait_for_edge(inp, GPIO_RISING)
+channel-fall = GPIO.wait_for_edge(inp, GPIO_FALLING)
 
 # Setup edge/event detection
 try:
